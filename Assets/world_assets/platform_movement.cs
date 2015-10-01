cae07a21bcbc17b274b7b60a7ba7ce227b1f225e
krp0005 <krp0005@uah.edu>
2015-10-01 12:37:45 -0500

    Created custom character movement script

---
+++ Assets/world_assets/platform_movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class platform_movement : MonoBehaviour {
+	public float moveSpeed = 3.0f;
+	private float moveDir;
+	// Use this for initialization
+	void Start () 
+	{
+		moveDir = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+}

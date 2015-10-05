855860741eb68eadb1d8cb8ab8f75dcfc3ac4b1c
krp0005 <krp0005@uah.edu>
2015-10-04 23:07:49 -0500

    Timer Work in Progress

--- Assets/world_assets/countdownTimer.cs
+++ Assets/world_assets/countdownTimer.cs
@@ -4,17 +4,14 @@ using UnityEngine.UI;
 
 public class countdownTimer : MonoBehaviour 
 {
-	public Text timerText = "Remaining Time";
+	public Text timerText;
 	public float timer = 15.0f;
-	public SpriteRenderer item_sr;
-	public PolygonCollider2D item_pc;
+
 
 	// Use this for initialization
 	void Start () 
 	{
 		timerText = GetComponent<Text> ();
-		item_sr = GetComponent<SpriteRenderer> ();
-		item_pc = GetComponent<PolygonCollider2D> ();
 	}
 	
 	// Update is called once per frame
@@ -22,15 +19,9 @@ public class countdownTimer : MonoBehaviour
 	{
 		timer -= Time.deltaTime;
 		timerText.text = "Remaining Time: " + timer.ToString ();
-    }
-
-	void OnTriggerEnter2D (Collider2D other)
-	{
-		if (other.gameObject.tag == "Player")
+		if (timer <= 0) 
 		{
-			item_sr.enabled = false;
-			item_pc.enabled = false;
-			timer += 10.0f;
-        }
+			Application.LoadLevel(2);
+		}
     }
 }
---
+++ Assets/world_assets/item_pickup.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class item_pickup : MonoBehaviour 
+{
+	public SpriteRenderer item_sr;
+	public PolygonCollider2D item_pc;
+
+	// Use this for initialization
+	void Start () 
+	{
+		item_sr = GetComponent<SpriteRenderer> ();
+		item_pc = GetComponent<PolygonCollider2D> ();
+	}
+	
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			item_sr.enabled = false;
+			item_pc.enabled = false;
+		}
+	}
+}
